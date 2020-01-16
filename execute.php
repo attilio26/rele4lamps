f9f41c61ca1e0f69d3c24e6161d1389a93c70c6a
attilio26 <39518843+attilio26@users.noreply.github.com>
2020-01-16 16:20:20 +0100

    Add files via upload

--- execute.php
+++ execute.php
@@ -165,7 +165,7 @@ elseif(strpos($text,"stato")){
 //<-- collegamento a dashboard Adafruit
 elseif(strpos($text,"ada")){
 	$response = file_get_contents("https://io.adafruit.com/attilio26/dashboards/esp01-2lamps");
-
+}
 //<-- reset modulo
 elseif(strpos($text,"reset")){
 	$response = file_get_contents("http://dario95.ddns.net:20083/rst");
