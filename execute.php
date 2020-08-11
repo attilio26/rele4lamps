2a50d81388894a8516f3bb09a26986bd6c4cf480
attilio26 <39518843+attilio26@users.noreply.github.com>
2020-08-11 09:26:02 +0200

    Add files via upload

--- execute.php
+++ execute.php
@@ -43,14 +43,7 @@ function clean_html_page($str_in){
 	$str_in = str_replace("<a href='?a="," ",$str_in);
 	$str_in = str_replace("r><h2>"," ",$str_in);
 	$str_in = str_replace(" </a></h2><h2>"," ",$str_in);
-	$str_in = str_replace("0'/>"," ",$str_in);
-	$str_in = str_replace("1'/>"," ",$str_in);
-	$str_in = str_replace("2'/>"," ",$str_in);
-	$str_in = str_replace("3'/>"," ",$str_in);	
-	$str_in = str_replace("4'/>"," ",$str_in);
-	$str_in = str_replace("5'/>"," ",$str_in);	
-	$str_in = str_replace("6'/>"," ",$str_in);
-	$str_in = str_replace("7'/>"," ",$str_in);	
+	$str_in = str_replace("/>"," ",$str_in);
 	return $str_in;
 }
 
